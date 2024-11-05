443a52c14269bbbbeaf2960e24f21cf82a629cba
Thomas Keim <thomashowardkeim@gmail.com>
2024-11-05 09:23:36 -0500

    New upload after computer wipe

---
+++ README.md
@@ -0,0 +1,1 @@
+This will be my first attempt at actually programming JavaScript. This is a Rock, Paper, Scissors (strike, throw, block) game that will be played exclusively in the console.
---
+++ rps.js
@@ -0,0 +1,46 @@
+/*function getComputerChoice (){
+  let rps = [Math.floor(Math.random()*3)];
+    if (rps == 0) {
+      console.log('rock');
+    } else if (rps == 1) {
+      console.log('paper');
+    } else if (rps == 2) {
+      console.log('scissors');
+    }
+    return 1;
+  }
+
+function getHumanChoice (){
+  var choice = prompt ('rock, paper, scissors?').toLowerCase();
+  console.log(choice); 
+  return choice;
+  }
+
+let humanScore = 0
+let computerScore = 0
+
+//const humanChoice = getHumanChoice ();
+//const computerChoice = getComputerChoice ();
+
+/*function playRound (){
+    function getComputerChoice (){
+      let rps = [Math.floor(Math.random()*3)];
+        if (rps == 0) {
+          console.log('rock');
+        } else if (rps == 1) {
+          console.log('paper');
+        } else if (rps == 2) {
+          console.log('scissors');
+        };
+      };
+    function getHumanChoice (){
+      var choice = prompt ('rock, paper, scissors?').toLowerCase();
+      console.log(choice); 
+      return choice;
+      };
+      const humanChoice = getHumanChoice;
+      const computerChoice = getComputerChoice;
+  if (humanChoice == 'rock' && computerChoice == 'rock') {
+  alert ('You both chose rock! Tie!')
+  };
+}*/
